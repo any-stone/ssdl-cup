d8d32014da84bc1c3f04339004b4ac8e0a8c287e
Annie Kuxa <kuxa.business@gmail.com>
2026-01-11 18:55:03 -0400

    added colors to the cup

--- main.cpp
+++ main.cpp
@@ -6,20 +6,31 @@
 
 int main (int argc, char** argv)
 {
-    sout << "Annie's favorite cup\n";
+    SSDL_SetWindowTitle ("Annie's favorite cup - in color");
     
-    // White part of the cup handle (we are drawing a white circle at this stage)
-    SSDL_RenderFillCircle(320, 230, 50);
+    //Setting up new colors for the picture
+    const SSDL_Color SAGE_GREEN = SSDL_CreateColor (158, 185, 168);
+    const SSDL_Color TERRACOTA = SSDL_CreateColor (204, 114, 101);
+    const SSDL_Color GHOST_WHITE = SSDL_CreateColor (248, 248, 255);
 
-    // Set the color to the screen color (black in our case) and drawing a "hole" in the cup handle
-    SSDL_SetRenderDrawColor(BLACK);
-    SSDL_RenderFillCircle(320, 230, 30);
-    
-    // Set the color to white and draw the body of the cup
-    SSDL_SetRenderDrawColor(WHITE);
-    SSDL_RenderFillRect(180, 140, 140, 180);
+    //Make a cream colored background
+    SSDL_RenderClear (TERRACOTA);
+
+    // Cup's handle
+    SSDL_SetRenderDrawColor     (SAGE_GREEN);
+    SSDL_RenderFillCircle       (320, 230, 50);
+
+    // Set the color to the background color and draw a "hole" in the cup handle
+    SSDL_SetRenderDrawColor     (TERRACOTA);
+    SSDL_RenderFillCircle       (320, 230, 30);
+
+    // Set the color back to sage green and draw the rest of the cup
+    SSDL_SetRenderDrawColor     (SAGE_GREEN);
+    SSDL_RenderFillRect         (180, 140, 140, 180);
 
     // Three white vertical lines to represent the steam from cup
+    SSDL_SetRenderDrawColor     (GHOST_WHITE);
+
     SSDL_RenderDrawLine(200, 130, 200, 100);
     SSDL_RenderDrawLine(250, 130, 250, 80);
     SSDL_RenderDrawLine(300, 130, 300, 100);
